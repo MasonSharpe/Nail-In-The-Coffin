36c31d58dd0182cb63a6bc6d9780b019b4f6dfc3
Kyan0ver9000 <115186321+Kyan0ver9000@users.noreply.github.com>
2024-05-14 13:59:35 -0700

    finished the ui setup

--- Assets/Kyans/VolumeSettings.cs
+++ Assets/Kyans/VolumeSettings.cs
@@ -24,7 +24,7 @@ public class VolumeSettings : MonoBehaviour
     public void SetMusicVolume()
     {
         float volume = musicSlider.value;
-        myMixer.SetFloat("music", Mathf.Log10(volume) * 20);
+        print(myMixer.SetFloat("music", Mathf.Log10(volume) * 20));
         PlayerPrefs.SetFloat("musicVolume", volume);
     }
     public void SetSFXVolume()
